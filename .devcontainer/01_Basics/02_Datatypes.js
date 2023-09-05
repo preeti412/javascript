1f3c3665115b35f247092d27fbf8433cd5d16719
preeti412 <136258835+preeti412@users.noreply.github.com>
2023-09-05 07:43:06 +0000

    container config

---
+++ .devcontainer/01_Basics/02_Datatypes.js
@@ -0,0 +1,38 @@
+"use strict";  //treat all js code as newer version
+
+//alert("hello")  //we are using node js not browser
+
+console.log(3
+    +3)
+    //code readabilty should be high 
+console.log("preeti")
+
+
+let name="preeti"
+let age=18
+let isLoggedin=false
+let state;
+
+
+
+
+//number=2to power 53
+//bigint
+//string==>""
+//boolean==>true/false
+//null=>stand alone value 
+//undefined==> value nt assigned
+//symbol ==> unieque 
+
+
+
+//object
+
+
+console.log(typeof  null)//object
+console.log(typeof  undefined)//undefined
+
+
+
+//https://tc39.es/ecma262/
+//mdn
