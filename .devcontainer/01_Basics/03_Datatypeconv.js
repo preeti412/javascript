29c5bea7c262357510a44f8ac81c8a71468f7aed
preeti412 <136258835+preeti412@users.noreply.github.com>
2023-09-05 08:02:07 +0000

    conversion of data type

---
+++ .devcontainer/01_Basics/03_Datatypeconv.js
@@ -0,0 +1,39 @@
+let score="33abc"
+
+
+console.log(typeof score)
+console.log(typeof (score))
+
+
+let valueInNumber=Number (score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)  //NaN
+
+
+
+//"33"==> 33
+//"33abc"==> NaN
+//true==>1
+//fals==>0
+
+let isLoggedin="pp"
+
+let booleanISloggedIN=Boolean(isLoggedin)
+console.log(booleanISloggedIN)
+
+
+
+//1==>true
+//0==>false
+//""==>false
+//"preeti"==>true
+
+
+
+let someno=33
+
+let stringno=String(someno)
+console.log(stringno)
+console.log(typeof stringno)
+
+
