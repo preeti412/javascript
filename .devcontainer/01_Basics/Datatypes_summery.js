855a1b985792c4d005dcef00b4211da25bcfa63f
preeti412 <136258835+preeti412@users.noreply.github.com>
2023-09-08 07:17:17 +0000

    ds

---
+++ .devcontainer/01_Basics/Datatypes_summery.js
@@ -0,0 +1,39 @@
+//Primitive
+
+//7types: String,Number,Boolean,null,undefined,Symbol,BignInt
+
+const score=100 //js is dynamically typed
+const scoreValue=100.3
+const isloggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id)
+
+//console.log(id===anotherId)
+
+
+const bigNumber=525254789541236555555n
+
+
+
+//Reference type (Non primitive):
+
+//Array,Objects
+
+const heros=["shaktiman","yash","doga"];
+let myObj={
+     name:"preeti",
+     age: 23,
+}
+
+const myfunction=function()
+{
+     console.log("hlo world")
+}
+
+
+
+console.log(typeof bigNumber)
