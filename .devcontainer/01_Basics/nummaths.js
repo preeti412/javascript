f5c5117372761f882d1ef4f00aa49cb72ab720db
preeti412 <136258835+preeti412@users.noreply.github.com>
2023-09-09 17:57:47 +0000

    mathnum

---
+++ .devcontainer/01_Basics/nummaths.js
@@ -0,0 +1,42 @@
+// const score=45
+// console.log(score)
+
+// const balance=new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+const otherNumber=123.8956
+
+// console.log(otherNumber.toPrecision(3))
+
+// const humdreds=1008000
+// console.log(humdreds.toLocaleString('en-IN'));
+
+//***************************MATHS***************************/
+
+// console.log(Math);
+// console.log(Math.abs(4))
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.2))
+// console.log(Math.abs(4))
+// console.log(Math.min(4,8,9,2))
+// console.log(Math.max(4,5,87))
+
+console.log(Math.random()) //math.random give values bw 0-1 
+console.log((Math.random()*10) + 1) //shifted values to left
+console.log(Math.floor(Math.random()*10)+1) //1-9 values
+
+
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()+(max-min+1))+min) //
+
+
